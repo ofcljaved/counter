a96595c7baa28b0add9bdbfdbebb646ea7e7eeb4
ofcljaved <javedofficialme@gmail.com>
2024-11-14 00:06:33 +0530

    chore: add counter program

--- programs/counter/src/lib.rs
+++ programs/counter/src/lib.rs
@@ -2,15 +2,50 @@ use anchor_lang::prelude::*;
 
 declare_id!("6dLCr2fhhBNRf5TUp64WXGaJ3jb1A8SD2tNnTk9TB6gC");
 
+pub const ANCHOR_DISCRIMINATOR_SIZE: usize = 8;
+
 #[program]
 pub mod counter {
     use super::*;
 
     pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
         msg!("Greetings from: {:?}", ctx.program_id);
+        let counter = &ctx.accounts.counter;
+        msg!("Counter accound created! Current count: {}", counter.count);
+        Ok(())
+    }
+
+    pub fn increment(ctx: Context<Increment>) -> Result<()> {
+        let counter = &mut ctx.accounts.counter;
+        msg!("Previous counter: {}", counter.count);
+
+        counter.count = counter.count.checked_add(1).unwrap();
+        msg!("Counter incremented! Current count: {}", counter.count);
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct Initialize<'info> {
+    #[account(mut)]
+    pub user: Signer<'info>,
+
+    #[account(
+        init,
+        payer=user,
+        space = 2 *  ANCHOR_DISCRIMINATOR_SIZE,
+    )]
+    pub counter: Account<'info, Counter>,
+    pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+pub struct Increment<'info> {
+    #[account(mut)]
+    pub counter: Account<'info, Counter>,
+}
+
+#[account]
+pub struct Counter {
+    pub count: u64,
+}
